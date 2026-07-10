00edc5b82c8e65ae7b3c35dd7058f63ddee10af1
whossatori <satoriyoxi@gmail.com>
2026-07-10 06:15:04 +0100

    Add cache busting to vite config

--- vite.config.js
+++ vite.config.js
@@ -16,4 +16,13 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].[hash].js`,
+        chunkFileNames: `assets/[name].[hash].js`,
+        assetFileNames: `assets/[name].[hash].[ext]`,
+      },
+    },
+  },
 })
